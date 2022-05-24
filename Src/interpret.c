494d2625c17b8325965851605bb3077ba914c942
Ryan Taylor Hathaway <hathaway6@llnl.gov>
2022-05-24 13:02:53 -0700

    Add sw commands to switch between primal and derived results

--- Src/interpret.c
+++ Src/interpret.c
@@ -2917,9 +2917,20 @@ parse_single_command( char *buf, Analysis *analy )
 				{
 					set_glyph_alignment( GLYPH_STAGGERED );
 				}
+				else if ( strcmp( tokens[i], "derived_results" ) == 0 ){
+					analy->result_source = DERIVED;
+					analy->result_mod = refresh_shown_result( analy );
+				}
+				else if ( strcmp( tokens[i], "primal_results" ) == 0 ){
+					analy->result_source = PRIMAL;
+					analy->result_mod = refresh_shown_result( analy );
+				}
+				else if ( strcmp( tokens[i], "all_results" ) == 0 ){
+					analy->result_source = ALL;
+					analy->result_mod = refresh_shown_result( analy );
+				}
 				else
-					popup_dialog( INFO_POPUP,
-								  "Switch command unrecognized: %s\n", tokens[i] );
+					popup_dialog( INFO_POPUP, "Switch command unrecognized: %s\n", tokens[i] );
 			}
 
 			/* Reload the result vector. */
