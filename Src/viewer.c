e9f3a833ba32e3863e32e9ec673e0d6222ebbc0c
James Durrenberger <durrenberger1@llnl.gov>
2022-06-28 13:03:37 -0700

    Resolve "on mat_labels as default behaviour"

--- Src/viewer.c
+++ Src/viewer.c
@@ -1957,12 +1957,6 @@ open_analysis( char *fname, Analysis *analy, Bool_type reload, Bool_type verify_
 		analy->mat_nums = forNums;
 		analy->mat_nums_reversed = revNums;
 
-		//default setting area
-
-		analy->sorted_labels = analy->sorted_nums;
-		analy->mat_labels = analy->mat_nums;
-		analy->mat_labels_reversed = analy->mat_nums_reversed;
-
 		// Make sure we get rid of dangling pointers
 		// DO NOT free this as it is now your analy->sorted_names pointer
 		// Simply setting it to NULL makes sure w do not have a dangling
@@ -2092,12 +2086,6 @@ open_analysis( char *fname, Analysis *analy, Bool_type reload, Bool_type verify_
 		analy->mat_nums = forNums;
 		analy->mat_nums_reversed = revNums;
 
-		//default setting area
-
-		analy->sorted_labels = analy->sorted_nums;
-		analy->mat_labels = analy->mat_nums;
-		analy->mat_labels_reversed = analy->mat_nums_reversed;
-
 		// Make sure we get rid of dangling pointers
 		// DO NOT free this as it is now your analy->sorted_names pointer
 		// Simply setting it to NULL makes sure w do not have a dangling
@@ -2105,6 +2093,13 @@ open_analysis( char *fname, Analysis *analy, Bool_type reload, Bool_type verify_
 		sortedNames = NULL;
 		sortedNums = NULL;
     }
+    
+    //default setting area
+    analy->sorted_labels = analy->sorted_names;
+    analy->mat_labels = analy->mat_names;
+	analy->mat_labels_reversed = analy->mat_names_reversed;
+
+		
     //init color storage
     analy->preview_mode = False;
     //last colors
